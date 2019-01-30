34772922a956d9d8fee47f748601d9b1bfd2010e
Kristen Allen <kmallen@butler.edu>
2019-01-30 10:18:48 -0500

    This commit adds the Stock table to the DB

---
+++ stock.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[itemID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [materialType] NCHAR(25) NOT NULL, 
+    [quantity] INT NOT NULL, 
+    [unitCost] MONEY NULL, 
+    [totalCost] MONEY NULL, 
+    [dateAcquired] DATE NULL, 
+    [dateUsed] DATE NULL
+)
