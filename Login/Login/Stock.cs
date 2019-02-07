50fc5ff748ac68b1df84753bea7c648667778c9b
Kristen Allen <kmallen@butler.edu>
2019-02-06 22:10:36 -0500

    Adds a Stocks class & blank reports form

---
+++ Login/Login/Stock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class Stock
+    {
+        public string materialType;
+        public double quantity, totalCost, defects;
+        public DateTime dateAcquired, dateUsed;
+
+        //returns the unit cost of materials purchased
+        public double unitCost()
+        {
+            return totalCost / quantity;
+        }
+
+        //returns the percent of unusable materials by dividing the defect number by quantity purchased
+        public string defectRate()
+        {
+            return (defects / quantity * 100).ToString() + "%";
+        }
+
+        //returns the time materials sit idle (difference between received date and use date)
+        public TimeSpan averageTime()
+        {
+            return dateUsed - dateAcquired;
+        }
+        
+
+    }
+}
