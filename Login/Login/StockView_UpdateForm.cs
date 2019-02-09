a2e14683903b0fda2c84f2348b0e870fdbff9ea9
Kristen Allen <kmallen@butler.edu>
2019-02-09 15:03:18 -0500

    Added validation to the Quantity field on the Add Materials form

---
+++ Login/Login/StockView_UpdateForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkflowManagement
+{
+    public partial class UpdateStock : Form
+    {
+        public UpdateStock()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateStock_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'workFlowDatabaseDataSet.StockTable' table. You can move, or remove it, as needed.
+            this.stockTableTableAdapter.Fill(this.workFlowDatabaseDataSet.StockTable);
+
+        }
+    }
+}
