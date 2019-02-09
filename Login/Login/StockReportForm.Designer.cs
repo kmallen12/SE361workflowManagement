449fdbd915eff6af9e98c4924a42143ef71e81da
Mherman1 <mherman1@butler.edu>
2019-02-09 14:13:21 -0500

    added View/Update Stock form and functionality (displays our database!! im a baller) and added very basic report form. connected those to our stock management homepage

--- Login/Login/StockReportForm.Designer.cs
+++ Login/Login/StockReportForm.Designer.cs
@@ -28,19 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_lowInventory = new System.Windows.Forms.Label();
+            this.lbl_maxCapacity = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // lbl_lowInventory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.lbl_lowInventory.AutoSize = true;
+            this.lbl_lowInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_lowInventory.Location = new System.Drawing.Point(37, 71);
+            this.lbl_lowInventory.Name = "lbl_lowInventory";
+            this.lbl_lowInventory.Size = new System.Drawing.Size(198, 18);
+            this.lbl_lowInventory.TabIndex = 1;
+            this.lbl_lowInventory.Text = "Items with Low Inventory:";
+            // 
+            // lbl_maxCapacity
+            // 
+            this.lbl_maxCapacity.AutoSize = true;
+            this.lbl_maxCapacity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_maxCapacity.Location = new System.Drawing.Point(37, 132);
+            this.lbl_maxCapacity.Name = "lbl_maxCapacity";
+            this.lbl_maxCapacity.Size = new System.Drawing.Size(240, 18);
+            this.lbl_maxCapacity.TabIndex = 2;
+            this.lbl_maxCapacity.Text = "Items Reaching Max Capacity: ";
+            // 
+            // StockReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.lbl_maxCapacity);
+            this.Controls.Add(this.lbl_lowInventory);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "StockReportForm";
             this.Text = "Stock Report";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_lowInventory;
+        private System.Windows.Forms.Label lbl_maxCapacity;
     }
 }
