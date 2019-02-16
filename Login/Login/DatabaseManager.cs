a6f4bd04661c82379007c80ac15be784cc6cb395
Mherman1 <mherman1@butler.edu>
2019-02-16 14:41:56 -0500

    added DatabaseManager class that works!

---
+++ Login/Login/DatabaseManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowManagement
+{
+    struct SqlQuery
+    {
+        string Table;
+        string Column;
+        string Where;
+        string Data;
+    }
+    static class DatabaseManager
+    {
+        private static SqlConnection _conn = new SqlConnection(
+            @"Server=tcp:workflowdatabase.database.windows.net,1433;
+              Initial Catalog=WorkFlowDatabase;
+              User ID=OCOTOD;
+              Password=FairBanks152;
+              Encrypt=true;"
+        );
+
+        public static SqlConnection conn
+        {
+            get { return _conn;} private set {conn = value;}
+        }
+
+        private static bool CheckField(string table, string column, string data)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select " + column + "from " + table + "where " + column + " = @field";
+            cmd.Parameters.AddWithBalue("@field", data);
+            try
+            {
+                conn.Open();
+                if (data.Equals((string)cmd.ExecuteScalar()))
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return false;
+        }
+
+        //below is the primary formatting of functions withtin this Database class
+        // think of it as an example. if u used it in other classes you'd scall it by: DatabaseManager.insertmaterial()
+
+        // public static void insertmaterial()
+        //{
+        //    try
+        //    {
+        //        conn.Open();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //        return;
+        //    }
+        //    finally
+        //    {
+        //        conn.Close();
+        //    }
+        //    MessageBox.Show("connected to db through database manager");
+        //    return;
+        //}
+    }
+}
